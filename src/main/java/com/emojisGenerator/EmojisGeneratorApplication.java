1a67a0baf76b103c4989cacecce949c1aeb37f95
Jeferson Bitencourt <jeferson267@hotmail.com>
2021-10-16 18:58:12 -0300

    First commit

---
+++ src/main/java/com/emojisGenerator/EmojisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.emojisGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojisGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmojisGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emojisGenerator/EmojisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emojisGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmojisGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
